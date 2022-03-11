8fa280bc58bcabec80ff9c532db2c562fd208ebd
Gamerhome1 <98109050+Gamerhome1@users.noreply.github.com>
2022-03-11 20:37:03 +0300

    Create newclient.py

---
+++ screens/newclient.py
@@ -0,0 +1,18 @@
+from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import SlideTransition
+
+
+class NewClient(Screen):
+  def id_client(self):
+    gs = gspread.service_account(filename='fileapi.json')
+    sht = gs.open_by_key('1a-dZsSTf4umMqdZJd3zax-9btQEcR1k1rDS7-4MGGoE')
+    self.worksheet = sht.sheet1
+    list_of_lists = self.worksheet.get_all_values()
+    self.id_client = len(list_of_lists)
+
+def fio(self):
+    self.someName = input("Введите ваше имя: ")
+    self.someS = input("Введите вашу фамилию: ")
+    self.somefather = input("Введите ваше отчество:")
+    self.sometel = input("Введите ваш телефон:")
+    def check_phone(self):
